7b3f6c071b1c676138ab6b918d5e7963bc3f512d
Claudia Villarroel <claudia.villarroel@live.com>
2023-06-30 00:03:48 -0400

    ADD: Archivos vistos en clase

---
+++ Clase_Javascript_01/assets/js/index.js
@@ -0,0 +1,29 @@
+/*let name="juan"; 
+console.log(name);
+
+let num1 = 10
+let num2 = 10
+
+let resultado = "hola"
+resultado = num1+num2;
+
+console.log(resultado);
+
+let nombre = prompt("Cual es tu nombre");
+console.log("El nombre es"+nombre);
+
+let edad = prompt("Cual es tu edad");
+console.log("El nombre es"+nombre + " y su edad es "+ edad);
+let edad2 = parseInt(edad);
+console.log(edad2);
+*/
+
+let variable = document.querySelector('h1');
+variable.innerText = "hola"
+variable.innerHTML = "Ya no soy un titulo"
+variable.style.color = "red"
+variable.style.fontSize = "50px"
+variable.style.fontFamily ="Courier New"
+
+let div = document.querySelector('div')
+div.innerText="Soy un div"
