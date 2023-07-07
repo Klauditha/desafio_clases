f545722b57576c1a35cb17ab16a169acc094f2fe
Claudia Villarroel <claudia.villarroel@live.com>
2023-07-06 23:42:05 -0400

    Condicion IF

---
+++ Clase_Javascript_02_Condiciones/index.js
@@ -0,0 +1,17 @@
+let parrafo = document.querySelector("p").innerHTML = 20;
+console.log(parrafo)
+
+let parrafo2 = document.querySelector("#parr2")
+console.log(parrafo2.innerHTML);
+
+let parrafo22 = document.getElementById("parr2")
+console.log(parrafo22.innerHTML);
+
+/*IF*/
+let edad=14
+if(edad<18){
+    alert("Eres menor de edad")
+}
+else{
+    alert("Eres mayor de edad")
+}
